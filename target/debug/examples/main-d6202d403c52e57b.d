adcaea066f451eaf7096df709d76f6b9bd30f55d
OmarDevX <omardevxme@gmail.com>
2024-07-10 15:24:09 +0300

    first commit

---
+++ target/debug/examples/main-d6202d403c52e57b.d
@@ -0,0 +1,7 @@
+/home/omar/Desktop/projects/prototypes/mandelbrot/target/debug/examples/libmain-d6202d403c52e57b.rmeta: examples/main.rs examples/window_manager/mod.rs examples/window_manager/window_manager.rs
+
+/home/omar/Desktop/projects/prototypes/mandelbrot/target/debug/examples/main-d6202d403c52e57b.d: examples/main.rs examples/window_manager/mod.rs examples/window_manager/window_manager.rs
+
+examples/main.rs:
+examples/window_manager/mod.rs:
+examples/window_manager/window_manager.rs:
